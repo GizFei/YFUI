61e61035c5631f97250cf53a81fe5f3c2ff03de4
GizFei <1150847818@qq.com>
2021-04-19 23:25:35 +0800

    更新 ColorPaintingView.kt

--- practice/src/main/java/com/giz/android/practice/widget/ColorPaintingView.kt
+++ practice/src/main/java/com/giz/android/practice/widget/ColorPaintingView.kt
@@ -4,10 +4,15 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.util.TypedValue
+import android.view.MotionEvent
 import android.view.View
+import androidx.annotation.ColorInt
 import com.giz.android.practice.R
+import kotlin.math.floor
 import kotlin.math.max
 import kotlin.math.min
 
@@ -46,7 +51,9 @@ class ColorPaintingView @JvmOverloads constructor(
 
     // 二维颜色数组
     private var mColorArray = Array(0) { IntArray(0) }
+    private val mDefaultBlockColor: Int = 0xFFDDDDDD.toInt()
 
+    // 方块画笔
     private val mBlockPaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     init {
@@ -59,7 +66,7 @@ class ColorPaintingView @JvmOverloads constructor(
             recycle()
         }
 
-        updateColorArray()
+        ensureColorArray()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -67,33 +74,6 @@ class ColorPaintingView @JvmOverloads constructor(
             getMeasuredSize(mDefaultWidth.toInt(), widthMeasureSpec, false),
             getMeasuredSize(mDefaultHeight.toInt(), heightMeasureSpec, true)
         )
-    }
-
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        super.onLayout(changed, left, top, right, bottom)
-
-        // 1、计算绘制宽高
-        mAreaWidth = width.toFloat() - paddingLeft - paddingRight
-        mAreaHeight = height.toFloat() - paddingTop - paddingBottom
-
-        // 2、计算绘制区域“零点”坐标、block宽高
-        mZeroX = paddingLeft.toFloat()
-        mZeroY = paddingTop.toFloat()
-        mBlockWidth = (mAreaWidth - mGapX * (mCols - 1)) / mCols
-        mBlockHeight = (mAreaHeight - mGapY * (mRows - 1)) / mRows
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        // 根据每个方块的坐标绘制
-        for (r in 0 until mRows) {
-            for (c in 0 until mCols) {
-                val left = (mBlockWidth + mGapX) * c + mZeroX
-                val top = (mBlockHeight + mGapY) * r + mZeroY
-
-                mBlockPaint.color = mColorArray.getOrNull(r)?.getOrNull(c) ?: Color.WHITE
-                canvas.drawRect(left, top, left + mBlockWidth, top + mBlockHeight, mBlockPaint)
-            }
-        }
     }
 
     /**
@@ -112,11 +92,99 @@ class ColorPaintingView @JvmOverloads constructor(
         }
     }
 
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+
+        // 1、计算绘制宽高
+        mAreaWidth = width.toFloat() - paddingLeft - paddingRight
+        mAreaHeight = height.toFloat() - paddingTop - paddingBottom
+
+        // 2、计算绘制区域“零点”坐标、block宽高
+        mZeroX = paddingLeft.toFloat()
+        mZeroY = paddingTop.toFloat()
+        mBlockWidth = (mAreaWidth - mGapX * (mCols - 1)) / mCols
+        mBlockHeight = (mAreaHeight - mGapY * (mRows - 1)) / mRows
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        Log.e("TAG", "onDraw: ")
+        // 根据每个方块的坐标绘制
+        ensureColorArray()
+        for (r in 0 until mRows) {
+            for (c in 0 until mCols) {
+                mBlockPaint.color = mColorArray[r, c]
+                canvas.drawRect(calcBlockRect(r, c), mBlockPaint)
+            }
+        }
+    }
+
+    private var mLastX = 0f
+    private var mLastY = 0f
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+//        when (event.action) {
+//            MotionEvent.ACTION_DOWN -> {
+//                mLastX = event.x
+//                mLastY = event.y
+//            }
+//            MotionEvent.ACTION_UP -> {
+//                val touchX = event.x - mZeroX
+//                val touchY = event.y - mZeroY
+//
+//                val c = floor(touchX / (mBlockWidth + mGapX)).toInt()
+//                val r = floor(touchY / (mBlockHeight + mGapY)).toInt()
+//                val blockRect = calcBlockRect(r, c)
+//                Log.e("TAG", "onTouchEvent: ($touchX, $touchY)  $blockRect")
+//                if (blockRect.contains(touchX, touchY)) {
+//                    ensureColorArray()
+//                    mColorArray[r][c] = Color.BLUE
+//                    invalidate()
+//                }
+//            }
+//        }
+
+        val touchX = event.x - mZeroX
+        val touchY = event.y - mZeroY
+
+        val c = floor(touchX / (mBlockWidth + mGapX)).toInt()
+        val r = floor(touchY / (mBlockHeight + mGapY)).toInt()
+        val blockRect = calcBlockRect(r, c)
+        if (blockRect.contains(touchX, touchY)) {
+            ensureColorArray()
+            if (mColorArray[r, c] != Color.BLUE) {
+                mColorArray[r, c] = Color.BLUE
+                invalidate()
+            }
+        }
+
+        return true
+    }
+
+    private fun calcBlockRect(row: Int, col: Int): RectF {
+        val left = (mBlockWidth + mGapX) * col + mZeroX
+        val top = (mBlockHeight + mGapY) * row + mZeroY
+
+        return RectF(left, top, left + mBlockWidth, top + mBlockHeight)
+    }
+
+
     /**
-     * 更新ColorArray
+     * 确保 [mColorArray] 的大小为 [mRows] * [mCols]
      */
-    private fun updateColorArray() {
-        mColorArray = Array(mRows) { IntArray(mCols) { Color.BLACK } }
+    private fun ensureColorArray() {
+        if (mColorArray.size != mRows || mColorArray[0].size != mCols) {
+            mColorArray = Array(mRows) { IntArray(mCols) { mDefaultBlockColor } }
+        }
+    }
+
+    @ColorInt
+    private operator fun Array<IntArray>.get(r: Int, c: Int): Int =
+        getOrNull(r)?.getOrNull(c) ?: mDefaultBlockColor
+
+    private operator fun Array<IntArray>.set(r: Int, c: Int, @ColorInt color: Int) {
+        if (isNotEmpty() && r in indices && c in get(0).indices) {
+            this[r][c] = color
+        }
     }
 
     private val Int.dp: Float get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
